cc252b9161737f08318f19bf59e12fdefc48f297
ElenaRusetskaya <michaelislen@gmail.com>
2023-10-22 21:42:54 +0300

    lesson21

---
+++ src/main/java/lesson21/util/HttpUtils.java
@@ -0,0 +1,26 @@
+package lesson21.util;
+
+import java.util.Arrays;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class HttpUtils {
+
+//    public static Map<String, String> parseUriQueryParams(String queryParam) {
+//        Map<String, String> paramMap = new HashMap<>();
+//        String[] params = queryParam.split("&");
+//        for (String param : params) {
+//            String[] split = param.split("=");
+//            String paramName = split[0];
+//            String paramValue = split[1];
+//            paramMap.put(paramName, paramValue);
+//        }
+//        return paramMap;
+//    }
+
+    public static Map<String, String> parseUriQueryParams(String queryParam) {
+        return Arrays.stream(queryParam.split("&"))
+                .map(str -> str.split("="))
+                .collect(Collectors.toMap(it -> it[0], it -> it[1], (a, b) -> b));
+    }
+}
