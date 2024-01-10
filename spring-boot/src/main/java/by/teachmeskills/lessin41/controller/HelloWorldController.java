4e965d2a831d85df976511d5c16726dea08c94b9
ElenaRusetskaya <michaelislen@gmail.com>
2024-01-10 19:20:00 +0300

    lesson41

--- spring-boot/src/main/java/by/teachmeskills/lessin41/controller/HelloWorldController.java
+++ spring-boot/src/main/java/by/teachmeskills/lessin41/controller/HelloWorldController.java
@@ -3,14 +3,16 @@ package by.teachmeskills.lessin41.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloWorldController {
-    @GetMapping("/user")
-    public String helloNewModule() {
-        return "user";
+    @GetMapping("/user/{name}")
+    public String view(@PathVariable("name") String name, Model model) {
+        model.addAttribute("", "Hello " + name);
+        return "/user";
     }
     @PostMapping("/helloSpringBoot")
     public String helloSpringBoot(@RequestParam String newModule, Model model) {
