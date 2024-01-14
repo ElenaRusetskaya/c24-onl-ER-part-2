47b0ad3ec56c93ae6080c2f74d788190cbad2c09
ElenaRusetskaya <michaelislen@gmail.com>
2024-01-14 13:16:31 +0300

    lesson40

---
+++ spring/src/main/java/by/teachmeskills/lesson40/service/UserService.java
@@ -0,0 +1,24 @@
+package by.teachmeskills.lesson40.service;
+
+import by.teachmeskills.lesson40.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
+import java.util.List;
+
+@Component
+public class UserService {
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+    public List<User> getAll() {
+        return jdbcTemplate.query("select * from users", new BeanPropertyRowMapper<>(User.class));
+    }
+    public User getOne(String email) {
+        return jdbcTemplate.query("select * from users where email = ?",
+                new BeanPropertyRowMapper<>(User.class)).stream().findAny().orElse(null);
+    }
+    public void add(User user) {
+        jdbcTemplate.update("select * from users", user.getName(), user.getPassword(), user.getEmail());
+    }
+}
