a9922556764bb0cc9d9cdab7d6e0c3d49f83bddc
ElenaRusetskaya <michaelislen@gmail.com>
2024-02-07 22:59:46 +0300

    lesson49

--- spring-boot/src/main/java/by/teachmeskills/lesson40/service/UserService.java
+++ spring-boot/src/main/java/by/teachmeskills/lesson40/service/UserService.java
@@ -1,6 +1,7 @@
 package by.teachmeskills.lesson40.service;
 
 import by.teachmeskills.lesson40.dto.UserDto;
+import by.teachmeskills.lesson40.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -13,12 +14,12 @@ import java.util.List;
 public class UserService {
     @Autowired
     private JdbcTemplate jdbcTemplate;
-    public List<UserDto> getAll() {
-        return jdbcTemplate.query("select * from users", new BeanPropertyRowMapper<>(UserDto.class));
+    public List<UserRepository> getAll() {
+        return jdbcTemplate.query("select * from users", new BeanPropertyRowMapper<>(UserRepository.class));
     }
-    public UserDto getOne(String email) {
+    public UserRepository getOne(String email) {
         return jdbcTemplate.query("select * from users where email = ?",
-                new BeanPropertyRowMapper<>(UserDto.class)).stream().findAny().orElse(null);
+                new BeanPropertyRowMapper<>(UserRepository.class)).stream().findAny().orElse(null);
     }
     public void add(@Valid UserDto userDto) {
         jdbcTemplate.update("select * from users", userDto.getName(), userDto.getPassword(), userDto.getEmail());
