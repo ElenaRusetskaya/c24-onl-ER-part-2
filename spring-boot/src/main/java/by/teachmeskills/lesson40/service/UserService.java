7634c7bceedf542a2a797c24fd279a29f8e94650
ElenaRusetskaya <michaelislen@gmail.com>
2024-02-14 15:19:50 +0300

    lesson49

--- spring-boot/src/main/java/by/teachmeskills/lesson40/service/UserService.java
+++ spring-boot/src/main/java/by/teachmeskills/lesson40/service/UserService.java
@@ -1,7 +1,6 @@
 package by.teachmeskills.lesson40.service;
 
 import by.teachmeskills.lesson40.dto.UserDto;
-import by.teachmeskills.lesson40.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
